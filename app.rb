bf47985ddba23596f5f052c66057185bc9d40a4e
Alexandre Gorius <ihkr@orange.fr>
2019-02-03 23:45:22 +0100

    amplitude ready

--- app.rb
+++ app.rb
@@ -24,40 +24,70 @@ post '/temp' do
   # parsing the csv
   csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
   csv_data = CSV.read(file)
-  headers = csv_data.shift
+  amplitude = csv_data[1][0].include? "Amplitude"
+  if amplitude
+    headers = csv_data[5]
+    csv_data.shift(6)
+  else
+    headers = csv_data.shift
+  end
   cohort_size = 0
 
   # getting absolute values to percents
   string_data = csv_data.map do |row|
 
     row.map.with_index do |cell, i|
-      if i == 0 then
-        cell.to_s
-      elsif i == 1 then
-        cell.to_i
-        cohort_size = cell.to_i
+      if amplitude
+        if i == 0 || i == 1 then
+          cell.to_s
+        elsif i == 2 then
+          cell.to_i
+          cohort_size = cell.to_i
+        else
+          (cell.to_f/cohort_size*100).round(2)
+        end
       else
-        (cell.to_f/cohort_size*100).round(2)
+        if i == 0 then
+          cell.to_s
+        elsif i == 1 then
+          cell.to_i
+          cohort_size = cell.to_i
+        else
+          (cell.to_f/cohort_size*100).round(2)
+        end
       end
     end
 
   end
   # Final hash converted from csv
   print array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
-
+  array_of_hashes = array_of_hashes.reverse if amplitude
+  # Getting all the nils as 0
+  array_of_hashes.map {|h| h.each{|k,v| h[k] = 0 unless v}}
   # Filling the weighted average hash with the right key/value skeleton
   weighted_average = array_of_hashes[0].clone
 
   # Computing the weighted average
+  if amplitude
+    segment_key = "Segment"
+    start_date_key="Start Date"
+    cohort_size_key="Users"
+  else
+    start_date_key="start date"
+    cohort_size_key="cohort size"
+  end
+
   weighted_average.each do |key, value|
     # setting variables
     week = key
     sum = 0
     product = 0
-    # Filling the two firsts columns
-    if key == "start date"
+    # Filling the two/three firsts columns
+    if key == segment_key
+      weighted_average[key] = ""
+    elsif key == start_date_key
       weighted_average[key] = "All time"
-    elsif key == "cohort size"
+    elsif key == cohort_size_key
       weighted_average[key] = 100
     # gathering the sum.prod and sum of weights
     else
@@ -66,8 +96,8 @@ post '/temp' do
           sum = sum
           product = product
         elsif line[week] != 0
-          sum += line["cohort size"]
-          product += line[week]*line["cohort size"]
+          sum += line[cohort_size_key]
+          product += line[week]*line[cohort_size_key]
         end
       # computing the weighted average
       weighted_average[key] = (product.to_f/sum.to_f).round(2)
